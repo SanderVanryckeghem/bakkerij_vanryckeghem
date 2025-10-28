ff8db358131e37281bd6e364aad4d0c623510908
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2025-10-28 08:45:45 +0100

    Remove openingshours from homepage and add button to link to over ons page

--- bakkerij_vanryckeghem/src/app/features/home/home/home.ts
+++ bakkerij_vanryckeghem/src/app/features/home/home/home.ts
@@ -1,16 +1,16 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Hero } from '../../../shared/components/hero/hero';
 import { CategoryCard } from '../../../shared/components/category-card/category-card';
 import { ContentService } from '../../../shared/services';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, CategoryCard],
+  imports: [Hero, CategoryCard, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   private contentService = inject(ContentService);
   bakeryInfo = this.contentService.getBakeryInfo();
-  openingHours = this.contentService.getOpeningHours();
 }
