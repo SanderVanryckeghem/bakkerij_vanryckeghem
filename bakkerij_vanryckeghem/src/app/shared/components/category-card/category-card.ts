5cdc92adc4d323db78f21f4098fa6b4eacb03d58
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2025-12-01 14:54:30 +0100

    Add svg to card

--- bakkerij_vanryckeghem/src/app/shared/components/category-card/category-card.ts
+++ bakkerij_vanryckeghem/src/app/shared/components/category-card/category-card.ts
@@ -1,9 +1,10 @@
 import { Component, input, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AssetUrlPipe } from '../../pipes/asset-url.pipe';
 
 @Component({
   selector: 'app-category-card',
-  imports: [RouterLink],
+  imports: [RouterLink, AssetUrlPipe],
   templateUrl: './category-card.html',
   styleUrl: './category-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,6 +13,7 @@ export class CategoryCard {
   title = input.required<string>();
   description = input.required<string>();
   imageUrl = input<string>();
+  icon = input<string>();
   routerLink = input<string>();
   queryParams = input<{ [key: string]: string }>();
 }
--- bakkerij_vanryckeghem/src/app/shared/models/category.model.ts
+++ bakkerij_vanryckeghem/src/app/shared/models/category.model.ts
@@ -1,6 +1,7 @@
 export interface Category {
   title: string;
   description: string;
+  icon?: string;
   routerLink: string;
   queryParams?: { [key: string]: string };
 }
