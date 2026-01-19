a4683d2e4a537517cb976911ce37b23c97df1b7f
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2026-01-19 16:02:07 +0100

    Update text owner

--- bakkerij_vanryckeghem/src/app/pages/bestellen/bestellen.ts
+++ bakkerij_vanryckeghem/src/app/pages/bestellen/bestellen.ts
@@ -23,17 +23,17 @@ export class Bestellen implements OnInit {
   orderingMethods: OrderingMethod[] = [
     {
       title: 'Telefonisch',
-      description: 'Bel ons op en bestel eenvoudig telefonisch. Wij helpen u graag met uw bestelling.',
+      description: 'Bel ons op, we helpen u graag met uw bestelling.',
       icon: 'M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z',
       actionText: '056 71 23 45',
       actionLink: 'tel:056712345'
     },
     {
       title: 'In de Winkel',
-      description: 'Kom gerust langs in onze winkel. U kunt ook ter plaatse bestellen voor later ophalen.',
+      description: 'Kom gerust langs in onze winkel.',
       icon: 'M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z',
       details: ['Overleiestraat 38', '8530 Harelbeke'],
-      actionText: 'Bekijk op kaart',
+      actionText: 'Routebeschrijving',
       actionLink: 'https://maps.google.com/?q=Overleiestraat+38+8530+Harelbeke'
     }
   ];
@@ -41,7 +41,7 @@ export class Bestellen implements OnInit {
   ngOnInit() {
     this.seoService.updateMetaTags({
       title: 'Bestellen - Bakkerij Vanryckeghem | Telefonisch of in de winkel',
-      description: 'Bestel uw vers brood en gebak bij Bakkerij Vanryckeghem. Telefonisch op 056 71 23 45 of kom langs in onze winkel.',
+      description: 'Bestel uw vers brood en gebak bij Bakkerij Vanryckeghem. Telefonisch op 056 71 23 45 of kom langs in onze winkel in Harelbeke.',
       keywords: 'bestellen, brood bestellen, gebak bestellen, telefonisch, winkel',
       url: 'https://www.bakkerijvanryckeghem.be/bestellen'
     });
